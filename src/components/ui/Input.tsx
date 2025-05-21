9128209c89b6417deaa9bd7badae6b167a0e4d66
Ovidiojcf <ojcfarias@gmail.com>
2025-05-21 18:05:36 -0300

    refactor(ui): aplicou-se classes mais simples e sem o ring

--- src/components/ui/Input.tsx
+++ src/components/ui/Input.tsx
@@ -5,7 +5,7 @@ import { cva, type VariantProps } from 'class-variance-authority';
 import { cn } from '../../../lib/utils';
 
 const inputVariants = cva(
-    'flex h-10 w-full rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background placeholder:text-muted-foreground focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50',
+    'flex h-10 w-full rounded-md border border-input bg-background px-3 py-2 text-sm text-black ring-offset-background placeholder:text-muted-foreground focus-visible:ring-1 disabled:cursor-not-allowed disabled:opacity-50',
     {
         variants:{
             inputSize:{
